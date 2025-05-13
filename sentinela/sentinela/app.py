bd2ef6ee517d9071e84048569725529b94cfbd9d
SofiaValadares <svc3@cesar.school>
2025-05-13 20:30:05 -0300

    connection test route

--- sentinela/sentinela/app.py
+++ sentinela/sentinela/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from routes import connection_test
+from sentinela.sentinela.routes import connection_test
 
 app = FastAPI()
 
-# Include routes
+# Include routes2
 app.include_router(connection_test.router)
